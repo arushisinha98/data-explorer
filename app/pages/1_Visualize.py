8d6577ae9d69bb4b24e78216043ecee3c93574c7
arushisinha98 <arushi@dsaid.gov.sg>
2024-03-24 16:23:40 +0800

    added plotly to requirements

--- app/pages/1_Visualize.py
+++ app/pages/1_Visualize.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
+import plotly.figure_factory as ff
 
 
 if __name__ == "__main__":
@@ -25,3 +26,57 @@ if __name__ == "__main__":
         visualize_df = st.session_state["FILTERED DATA"]
     
     st.write(f"Shape of selected data: {visualize_df.shape[0]} rows, {visualize_df.shape[1]} columns")
+    
+    with st.expander("View Data"):
+        st.dataframe(visualize_df)
+    
+    OneD, TwoD, ThreeD = st.tabs(["  1D  ","  2D  ","  3D  "])
+    dtypes = dict(visualize_df.dtypes)
+    
+    with OneD:
+        # select x-axis
+        x_axis = st.selectbox(label = "Select x-axis",
+                              options = list(visualize_df.columns),
+                              index = None,
+                              key = "1D_x_axis")
+        
+        # select group_by (must be categorical)
+        group_by = st.multiselect("Select Column(s) to Group By",
+                                  options = [col for col in visualize_df.columns if col not in x_axis])
+        
+        if x_axis:
+            if dtypes[x_axis] == "string[python]" or dtypes[x_axis] == "boolean":
+                st.write("INSERT BAR CHART")
+            else:
+                st.write("INSERT HISTOGRAM")
+                
+    with TwoD:
+        # select x-axis (must be numeric)
+        x_axis = st.selectbox(label = "Select x-axis",
+                              options = list(visualize_df.columns),
+                              index = None,
+                              key = "2D_x_axis")
+                              
+        # select y-axis (must be numeric)
+        y_axis = st.selectbox(label = "Select y-axis",
+                              options = [col for col in visualize_df.columns if col not in x_axis],
+                              index = None,
+                              key = "2D_y_axis")
+        
+        # select group_by (must be categorical)
+        group_by = st.multiselect("Select Column(s) to Group By",
+                                  options = [col for col in visualize_df.columns if col not in x_axis and col not in y_axis])
+        
+        # if x_axis and y_axis:
+    
+    with ThreeD:
+        # select x-axis (must be numeric)
+        
+        # select y-axis (must be numeric)
+        
+        # select z-axis
+        
+        # select group_by (must be categorical)
+        
+        # if x_axis and y_axis and z_axis:
+        
